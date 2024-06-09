bec46e9a9e69f992f983e5f21ed7feb5f2e519c8
1Ashutosh1 <at8969927@gmail.com>
2024-06-09 12:09:49 +0530

    create get users controller

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+
 import connectToMongoDB from './db/connectToMongoDB.js';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +19,8 @@ app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
+app.use('/api/users',userRoutes);
+
 
 // app.get('/', (req,res) => {
 //   res.send("Server is running!!");
